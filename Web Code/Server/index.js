dc2de92f637baf45ea1f2bae968b81bc1de6c0d6
oxxeys <147168632+oxxeys@users.noreply.github.com>
2026-04-07 16:04:52 +0100

    Update index.js

---
+++ Web Code/Server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
