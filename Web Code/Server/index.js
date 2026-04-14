4fc16ad11e4d5a3073b80951ce41479f568fe7d3
oxxeys <147168632+oxxeys@users.noreply.github.com>
2026-04-14 21:44:36 +0100

    Worked On Web Server Code
    This code is for a diffrent project but is being uploaded here as I need to test it through render

--- Web Code/Server/index.js
+++ Web Code/Server/index.js
@@ -4,10 +4,46 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/data", (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://kiran1104_db_user:VsxQ2SCnphgCEGVY@floridium.vkjfn7c.mongodb.net/floridium?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch(err => {
+    console.log("Can't find the database!", err)
+    process.exit();
+  })
+
+const plantDataSchema = new mongoose.Schema({
+  box_id: req.body.box_id,
+  moisture_reading: req.body["Soil Moisture"],
+  temprature_reading: req.body.Temprature,
+  sunlight_reading: req.body.Sunlight,
+  reading_time: { type: Date, default: Date.now }
+})
+
+const data = mongoose.model("data", plantDataSchema);
+
+app.post("/data", async (req, res) => {
+  
+  try{
+    console.log(req.body);
+    const newData = new data(req.body);
+    await newData.save();
+    res.send("Saved To DB");
+
+  }
+  catch (err){
+    console.error(err);
+    res.status(500).send("Error saving data");
+  }
+  
+
+
+  
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
