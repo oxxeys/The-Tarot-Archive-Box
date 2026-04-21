a90ec8b67fa1cc1811ae1f76045f02b10314850c
oxxeys <147168632+oxxeys@users.noreply.github.com>
2026-04-21 14:17:47 +0100

    Added Bootstrap

--- Web Code/Server/index.js
+++ Web Code/Server/index.js
@@ -1,8 +1,13 @@
+// set up express
 const express = require("express");
 const path = require("path");
 const app = express();
 
-//Connect to mongo
+//setup bootstrap 
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+
+
+// connect to mongo
 const dotenv = require('dotenv').config()
 const mongoUsername=process.env.mongoUsername
 const mongoPassword=process.env.mongoPassword
@@ -26,9 +31,6 @@ app.use(session({
   cookie:{maxAge: 5*60*1000 }
 }))
 
-
-
-
 //serve files from /public 
 app.use(express.static('public'))
 
@@ -41,35 +43,39 @@ app.use(express.json());
 const port = process.env.PORT || 3000; // set like this to work with Render
 app.listen(port, () => console.log(`Server running on ${port}`));
 
+// function to check if the user is logged in before letting them access  
 function checkIfLoggedIn(request, response, nextAction){
   if(request.session){
         if(request.session.username){
             nextAction()
         }else{
             request.session.destroy()
-            response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+            response.sendFile(path.join(__dirname, '/views', 'login.html'))
         }
     }
+    // if not logged in, send to notLogged
     else{
         request.session.destroy()
-        response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+        response.sendFile(path.join(__dirname, '/views', 'login.html'))
     } 
 }
 
+// post request when user submits login form
 app.post(`/login`, async (request, response)=>{
   if(await userModel.checkUser(request.body.username, request.body.password)){
         request.session.username=request.body.username
         response.sendFile(path.join(__dirname, '/views', 'app.html'))
     } else{
-        response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+        response.sendFile(path.join(__dirname, '/views', 'login.html'))
     }
 })
 
+// user requests /login end point
 app.get('/login', (request, response)=>{
     response.sendFile(path.join(__dirname, '/views', 'login.html'))
 })
 
-
+// user requests /app end point
 app.get('/app', checkIfLoggedIn, (req, res) => {
   res.send(`Welcome ${req.session.username}`);
 });
